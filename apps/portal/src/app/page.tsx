ef13a0623071a81ce3d1ace756225b4f2d4cde73
Marcos Neto <marcos_nto@hotmail.com>
2025-10-26 02:11:53 -0300

    feat(apps/portal): :tada: add portal

---
+++ apps/portal/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@workspace/ui/components/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col gap-4 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Painel
+      <Button size="sm">Button</Button>
+    </div>
+  );
+}
